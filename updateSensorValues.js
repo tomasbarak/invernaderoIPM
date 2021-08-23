dd60f9eeeefde0a8627084eaa6cdd80e208ed414
tomasbarak <barakcinthia@gmail.com>
2021-08-22 23:05:03 -0300

    first commit

---
+++ updateSensorValues.js
@@ -0,0 +1,12 @@
+
+    var starCountRef = firebase.database().ref('Sensors/');
+    starCountRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        useSensorsData(data);
+    });
+
+    function useSensorsData(data){
+        document.getElementById('ambientLightVal').style.height = "" + (Number(data.AmbientLight) / 1023)*100 + "%";
+        console.log("" + (Number(data.AmbientLight) / 1023)*100) ;
+    }
