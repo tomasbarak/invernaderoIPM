ab27eb7d4f7a0aed586683355477e1532202bb21
tomasbarak <barakcinthia@gmail.com>
2021-08-23 02:04:11 -0300

    resizing

--- updateSensorValues.js
+++ updateSensorValues.js
@@ -7,7 +7,7 @@
     });
 
     function useSensorsData(data){
-        document.getElementById('ambientLightVal').style.height = "" + (Number(data.AmbientLight) / 85)*100 + "%";
+        document.getElementById('ambientLightVal').style.height = "" + (Number(data.AmbientLight) / 1023)*100 + "%";
         document.getElementById('ambientTempVal').style.height = "" + (Number(data.AmbientTemp) / 85)*100 + "%";
         document.getElementById('floorHumidityVal').style.height = "" + (Number(data.FloorHumidity) / 100)*100 + "%";
         console.log("" + (Number(data.AmbientLight) / 85)*100) ;
