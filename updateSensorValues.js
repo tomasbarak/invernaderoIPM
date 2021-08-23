4a58587529febe2a3c928e758aa8566b47d5ef89
tomasbarak <barakcinthia@gmail.com>
2021-08-22 23:07:28 -0300

    Second commit

--- updateSensorValues.js
+++ updateSensorValues.js
@@ -8,5 +8,5 @@
 
     function useSensorsData(data){
         document.getElementById('ambientLightVal').style.height = "" + (Number(data.AmbientLight) / 1023)*100 + "%";
-        console.log("" + (Number(data.AmbientLight) / 1023)*100) ;
+        console.log("" + (Number(data.AmbientLight) / 255)*100) ;
     }
